83bd4837cee1be1424339bd1b2e0f436609d2f9c
Gabriel Salasario <salasario346827@gmail.com>
2023-08-09 01:51:44 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddTaks = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(46, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 24);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnAddTaks
+            // 
+            this.btnAddTaks.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddTaks.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddTaks.Location = new System.Drawing.Point(46, 90);
+            this.btnAddTaks.Name = "btnAddTaks";
+            this.btnAddTaks.Size = new System.Drawing.Size(214, 37);
+            this.btnAddTaks.TabIndex = 4;
+            this.btnAddTaks.Text = "Adicionar Tarefa";
+            this.btnAddTaks.UseVisualStyleBackColor = true;
+            this.btnAddTaks.Click += new System.EventHandler(this.btnAddTaks_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(46, 179);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(214, 259);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAddTaks);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddTaks;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+       public List<string> tasksToDoList = new List<string>(){"Carlos", "Ana", "Joao"};
+        public void RefreshFlowLayoutPanel1(){
+            flowLayoutPanel1.Controls.Clear();
+        }
+        public void ShowTasks()
+        {
+            for (int i = 0; i < tasksToDoList.Count; i++)
+            {
+                Label taskLabel = new Label();
+                Button buttonDeleteTask = new Button();
+
+                taskLabel.Text = tasksToDoList[i];
+                taskLabel.AutoSize = true;
+                taskLabel.Font = new Font("Arial", 12, FontStyle.Regular);
+
+                buttonDeleteTask.Text = "X";
+                buttonDeleteTask.Tag = tasksToDoList[i];
+                buttonDeleteTask.Click += ButtonDeleteTask_Click;
+                flowLayoutPanel1.SetFlowBreak(buttonDeleteTask, true);
+                flowLayoutPanel1.Controls.Add(taskLabel);
+                flowLayoutPanel1.Controls.Add(buttonDeleteTask);
+               
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddTaks_Click(object sender, EventArgs e)
+        {
+          int inputTxtLength = textBox1.Text.Length;
+            if(inputTxtLength >= 1) { 
+            tasksToDoList.Add(textBox1.Text);
+            RefreshFlowLayoutPanel1();
+            ShowTasks();
+            textBox1.Text = "";
+            }
+        }
+
+        private void lblTest_Click(object sender, EventArgs e)
+
+        {
+            
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowTasks();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+        
+        }
+        private void ButtonDeleteTask_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string taskToRemove = (string)button.Tag;
+            tasksToDoList.Remove(taskToRemove);
+            RefreshFlowLayoutPanel1();
+            ShowTasks();
+            
+        }
+    }
+}
